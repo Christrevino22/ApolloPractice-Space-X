41a07ddcf0794f7ac1375c3db6a3ed58f8b957a9
chris <chrisdadef@gmail.com>
2022-04-14 21:35:50 -0500

    added a few more features

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client'
+import React, { useState } from 'react'
+import { gql, useQuery, useLazyQuery } from '@apollo/client'
 
 const QUERY_ALL_ROCKETS = gql`
     query GetAllRockets {  
@@ -13,8 +13,26 @@ const QUERY_ALL_ROCKETS = gql`
 
 `
 
+const QUERY_ALL_DRAGONS = gql`
+    query GetAllDragons {  
+      dragons {
+    name
+    type
+    description
+    active
+    crew_capacity
+  }
+}
+
+`
+
 const DisplayData = () => {
+  
+  const [dragonSearched, setDragonSearched] = useState("")
+  
   const {data, loading, error} = useQuery(QUERY_ALL_ROCKETS);
+  const {data: dragonData} = useQuery(QUERY_ALL_DRAGONS);
+
 
 
   if (loading) {
@@ -38,16 +56,48 @@ const DisplayData = () => {
             <h1 key= {idx}> </h1>
             <h1>Name: {rockets.name}</h1>
             <h1>Description: {rockets.description}</h1>
-            <h1>First Flight: {rockets.first_flight}</h1>
-
-            
-            
-
+            <h1>Active: {rockets.active}</h1>
     </div>
           );
-    })}
+
+          
+        
+       })}
+
+<br></br>
+<br></br>
+<h1>Below is the dragons!!</h1>
+<br></br>
+<br></br>
+
+
+
+       {dragonData && dragonData.dragons.map((drg, idx) => {
+         return (
+           <div> 
+              <div key={idx}></div>             
+              <h1>Name:{drg.name}</h1>
+              <h1>Description:{drg.description}</h1>
+              <h1>Crew Compacity:{drg.crew_capacity}</h1>
+              <h1>Active:{drg.active}</h1>
+
+
+           </div>
+         )
+       })}
+         <div>
+            <input type="text" 
+            placeholder="Intersellar" 
+            onChange={(event) => {
+              console.log("this has worked......")
+            setDragonSearched(event.target.value) ; 
+            }}></input>
+              <button>Fetch data</button>
+                
+          </div>
+
               </div>
-  )
+  );
 }
 
 export default DisplayData
